0de7e7ff545aaa9a9ba7bce8946fb81fa66381c8
Rodrigo Torrejon <torrejonrodrigo@gmail.com>
2025-05-29 09:15:14 -0400

    Agrega CrossOrigin

--- productos/src/main/java/com/application/productos/controller/ProductoController.java
+++ productos/src/main/java/com/application/productos/controller/ProductoController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/productos")
 @Tag(name = "Product API")
+@CrossOrigin
 public class ProductoController {
     @Autowired
     private ProductoService productoService;
